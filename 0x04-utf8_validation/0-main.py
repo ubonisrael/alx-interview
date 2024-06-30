18deeaf50fcaa4bc99d81e2d076a91848e0f62cc
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-30 23:01:41 +0100

    0x04 added checks for null

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -13,3 +13,15 @@ print(validUTF8(data))
 
 data = [229, 65, 127, 256]
 print(validUTF8(data))
+
+data = [0, 0, 0, 0, 0]
+print(validUTF8(data))
+
+data = []
+print(validUTF8(data))
+
+data = []
+#print(validUTF8(data))
+
+data = []
+#print(validUTF8(data))
