cbdbdcd7196de6b8c4e17f4740fd4e2785cd2ab3
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-24 16:23:04 +0100

    0x08 initial attempt

---
+++ 0x08-making_change/0-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+import time
+
+makeChange = __import__('0-making_change').makeChange
+
+st = time.time()
+print(makeChange([10, 15, 5], 50))
+end1 = time.time()
+print(makeChange([1, 2, 25], 37))
+end2 = time.time()
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+end3 = time.time()
+print(end1 - st)
+print(end2 - end1)
+print(end3 - end2)
---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""contains the making_change function"""
+
+
+def makeChange(coins, total):
+    """"""
+    coins.sort(reverse=True)
+    return makeChangeHelper(coins, total)
+
+
+def makeChangeHelper(coins, current_total):
+    """"""
+    res, mod = getDividendAndMod(current_total, coins[0])
+
+    if mod == 0:
+        return res
+    
+    # check if this is the last coin
+    if mod != 0 and len(coins) == 1:
+        return -1
+    
+    if res == 0:
+        return makeChangeHelper(coins[1:], mod)
+    
+    arr = []
+    for x in range(res + 1):
+        possible_min = makeChangeHelper(coins[1:], x * coins[0] + mod)
+        if possible_min == -1:
+            continue
+        arr.append(res - x + possible_min)
+    return min(arr) if len(arr) > 0 else -1
+
+
+def getDividendAndMod(quotient, divisor):
+    """"""
+    return (quotient // divisor, quotient % divisor)
